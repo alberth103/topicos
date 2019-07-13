e44cecf895b4896fd2fdcc1f187836815774449e
alberth103 <alberth103@gmail.com>
2019-07-12 18:10:08 -0600

    first upload

---
+++ Ahorcado/AhorcadoService/AhorcadoService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AhorcadoService
+{
+    [DataContract]
+    public class Name
+    {
+        [DataMember]
+        public string First;
+        [DataMember]
+        public string Last;
+    }
+
+    [ServiceContract]
+    public interface IAhorcado
+    {
+        [OperationContract]
+        string SayHello(Name person);
+    }
+
+    public class AhorcadoService : IAhorcado
+    {
+        public string SayHello(Name person)
+        {
+            return string.Format("Hello {0} {1}",
+            person.First, person.Last);
+        }
+    }
+}
